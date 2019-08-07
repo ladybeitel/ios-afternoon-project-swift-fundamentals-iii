a8ad39540c57ccc635e54d9949edfba155c48393
Ciara Beitel <ciarabeitel@gmail.com>
2019-08-07 17:24:45 -0400

    Completed project and strech goals

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -5,8 +5,16 @@
 //  Created by Ben Gohlke on 8/7/19.
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
+// Ciara "CC" Beitel
 
 import UIKit
+
+enum Currency {
+    case cad
+    case mxn
+}
+
+var currency: Currency = .cad
 
 class ViewController: UIViewController {
     
@@ -19,21 +27,56 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var cadButton: UIButton!
     @IBOutlet weak var mxnButton: UIButton!
-
+    
     // MARK: - Actions
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
+        guard let userInput = fromCurrencyTextField.text else { return }
+        guard let userInputDouble = Double(userInput) else { return }
         
+        let currencyFormatter: NumberFormatter = {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .currency
+            return formatter
+        }()
+        
+        let convertedCurrency = convertCurrency(userInputDouble)
+        let formattedCurrency = currencyFormatter.string(from: NSNumber(value: convertedCurrency))
+        toCurrencyTextField.text = formattedCurrency
     }
     
+    
     @IBAction func cadButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        mxnButton.isSelected.toggle()
         
+        if cadButton.isSelected {
+            toCurrencyLabel.text = "Currency (CAD)"
+        }
     }
     
     @IBAction func mxnButtonTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        cadButton.isSelected.toggle()
         
+        if mxnButton.isSelected {
+            toCurrencyLabel.text = "Currency (MXN)"
+        }
     }
     
     // MARK: - Helper Methods
+    
+    
+    
+    func convertCurrency(_ dollars: Double) -> Double {
+        if cadButton.isSelected {
+            let convertMoney = dollars * 1.33
+            return convertMoney
+        } else {
+            let convertMoney = dollars * 19.70
+            return convertMoney
+        }
+    }
+    
 }
 
